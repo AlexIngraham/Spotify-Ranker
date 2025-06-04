7da1178aa17a66df2c701c815e4d1365e92eaef8
Alex Ingraham <129226541+AlexIngraham@users.noreply.github.com>
2025-06-04 11:57:26 -0400

    Ranked artists, printed to terminal

--- read_csv.py
+++ read_csv.py
@@ -26,24 +26,69 @@ def load_csv(csv_file, db_file):
             skipped_tracks INTEGER DEFAULT 0
         );
         ''')
+        cursor.execute('''
+                      UPDATE userInformation
+                      SET utc_time = substr(utc_time, 1, instr(utc_time, ',') -1)
+                      WHERE instr(utc_time, ',') > 0;
+                      ''')
+        conn.commit()
 
-def get_top_artist(cursor):
-    df = pd.read_csv(csv_file)
-    df.to_sql('userInformation', conn, if_exists='replace', index=False)
+def get_top_artist(cursor, limit = 10):
     query = '''
     SELECT artist, COUNT(*) as times_played
     FROM userInformation
     GROUP BY artist
     ORDER BY times_played DESC
-    LIMIT 1;
+    LIMIT ?;
     '''
-    cursor.execute(query)
+    cursor.execute(query, (limit,))
+    return cursor.fetchall()
 
-    
+def get_top_tracks(cursor, limit = 10):
+    query = '''
+    SELECT track, COUNT(*) as times_played
+    FROM userInformation
+    GROUP BY track
+    ORDER BY times_played DESC
+    LIMIT ?;
+    '''
+    cursor.execute(query, (limit,))
+    return cursor.fetchall()
 
+def get_top_albums(cursor, limit = 10):
+    query = '''
+    SELECT album, COUNT(*) as times_played
+    FROM userInformation
+    GROUP BY album
+    ORDER BY times_played DESC
+    LIMIT ?;
+    '''
+    cursor.execute(query, (limit,))
+    return cursor.fetchall()
+
+def most_played_days(cursor, limit = 10):
+    query = '''
+    SELECT utc_time, COUNT(*) as times_played
+    FROM userInformation
+    GROUP BY utc_time
+    ORDER BY times_played DESC
+    LIMIT ?;
+    '''
+    cursor.execute(query, (limit,))
+    return cursor.fetchall()
 
 load_csv(csv_file, db_file)
 get_top_artist(cursor)
+get_top_tracks(cursor)
+get_top_albums(cursor)
+
+print("Top Artists: :", get_top_artist(cursor))
+print("\n")
+print("Top Tracks: :", get_top_tracks(cursor))
+print("\n")
+print("Top Albums: :", get_top_albums(cursor))
+print("\n")
+print("Most Played Days: :", most_played_days(cursor))
 
 conn.commit()
 conn.close()
